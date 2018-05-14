4b81119c6b5af1a9da5001b2a7a172cd20e12603
James Collins <jamescol@usc.edu>
2018-05-14 15:24:11 -0700

    Finish and tested input handling and event handling. Moving on to basic movement in the character controller

---
+++ Plasticity/Assets/Scripts/EventManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// This script is based on the tutorial, "Events: Creating a Simple Messaging System" made by Adam Buckner.
+// https://unity3d.com/learn/tutorials/topics/scripting/events-creating-simple-messaging-system
+
+//Need to create an event class that extends UnityEvent<int> so that we can create events that actually pass information
+[System.Serializable]
+public class InputEvent : UnityEvent<int>
+{
+
+}
+
+
+public class EventManager : MonoBehaviour
+{
+    //Event dictionary will associate a string name with a specific InputEvent object
+    private Dictionary<string, InputEvent> EventDictionary;
+
+    //CurrentManager is an instance of the event manager script
+    private static EventManager CurrentManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            //If we do not have a current instance of the event manager...
+            if (!CurrentManager)
+            {
+                //Look for an instance of the event manager in our scene
+                CurrentManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!CurrentManager)
+                {
+                    //If we cannot find an event manager in the scene, we got a problem
+                    Debug.LogError("There is not currently an event manager in the scene");
+                }
+                else
+                {
+                    //If we find an instance of the event manager, initialize it
+                    CurrentManager.Init();
+                }
+            }
+            //return the instance of the event manager
+            return CurrentManager;
+        }
+    }
+
+    void Init()
+    {
+        if(EventDictionary == null)
+        {
+            //If we don't have a dictionary defined in EventDictionary, create a new one
+            EventDictionary = new Dictionary<string, InputEvent>();
+        }
+    }
+
+    public static void StartListening(string EventName, UnityAction<int> Listener)
+    {
+        InputEvent ThisEvent = null;
+        //If the we are looking for exists within our dictionary, put it in the variable ThisEvent
+        if (instance.EventDictionary.TryGetValue(EventName, out ThisEvent))
+        {
+            //Add the new listener to the event
+            ThisEvent.AddListener(Listener);
+        }
+        //If the event we are looking for does not yet exist in our dictionary...
+        else
+        {
+            //Create a new event, add a listener, and place it in our EventDictionary
+            ThisEvent = new InputEvent();
+            ThisEvent.AddListener(Listener);
+            instance.EventDictionary.Add(EventName, ThisEvent);
+        }
+    }
+
+    public static void StopListening(string EventName, UnityAction<int> Listener)
+    {
+        //If the current instance of the manager doesn't exist we don't need to do anything
+        if (CurrentManager == null) return;
+        InputEvent ThisEvent = null;
+        if (instance.EventDictionary.TryGetValue(EventName, out ThisEvent))
+        {
+            //Need to remove listener from event in dictionary if we find it 
+            ThisEvent.RemoveListener(Listener);
+        }
+    }
+
+    public static void TriggerEvent(string EventName, int value)
+    {
+        InputEvent ThisEvent = null;
+        if (instance.EventDictionary.TryGetValue(EventName, out ThisEvent))
+        {
+            //If the event exists in our dictionary, invoke it
+            ThisEvent.Invoke(value);
+        }
+    }
+}
---
+++ Plasticity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour {
+
+    /*
+     * The Input manager is the first place player input is registered. Key bindings can be adjusted in the editor without 
+     * needing to update any code. The Input Manager fires events that are managed by the Event Manager and listened to by the 
+     * Character Controller. This way, any object in the game can listen to input events, and the back end of where we are listening 
+     * for input is all in the same place. 
+     */
+
+    [SerializeField]
+    [Tooltip("Keys that will be bound to the move up action")]
+    private KeyCode[] MoveUp;
+    [SerializeField]
+    [Tooltip("Keys that will be bound to the move left action")]
+    private KeyCode[] MoveLeft;
+    [SerializeField]
+    [Tooltip("Keys that will be bound to the move right action")]
+    private KeyCode[] MoveRight;
+    [SerializeField]
+    [Tooltip("Keys that will be bound to the move down action")]
+    private KeyCode[] MoveDown;
+    [SerializeField]
+    [Tooltip("Keys that will be bound to the interact action")]
+    private KeyCode[] Interact;
+
+    /*
+     * Need integers to store number of presses active at one time. If both W and UP ARROW are pressed at the same
+     * time, but only one is released, we don't want the player to stop moving. This way, the key event only triggers with
+     * value 1 when we want the player to start moving, and triggers with value 0 when we want them to stop, completely
+     * independent of whether one specific key is up or down. 
+     */
+    private int UpPresses = 0;
+    private int DownPresses = 0;
+    private int LeftPresses = 0;
+    private int RightPresses = 0;
+    private int InteractPresses = 0;
+
+
+    void Start () {
+
+	}
+	
+	void Update () {
+        UpKeys();
+        LeftKeys();
+        RightKeys();
+        DownKeys();
+        InteractKeys();
+	}
+
+
+    private void UpKeys()
+    {
+        for (int i = 0; i < MoveUp.Length; ++i)
+        {
+            if (Input.GetKeyDown(MoveUp[i]))
+            {
+                //If no other keys have triggered the up event and an up key is pressed, trigger pressed event
+                if (UpPresses == 0)
+                {
+                    EventManager.TriggerEvent("UpKey", 1);
+                }
+                ++UpPresses;
+            }
+
+            if (Input.GetKeyUp(MoveUp[i]))
+            {
+                //If there aren't any other pressed up keys when we release an up key, trigger released event
+                if (UpPresses == 1)
+                {
+                    EventManager.TriggerEvent("UpKey", 0);
+                }
+                --UpPresses;
+            }
+        }
+    }
+
+    private void LeftKeys()
+    {
+        for (int i = 0; i < MoveLeft.Length; ++i)
+        {
+            if (Input.GetKeyDown(MoveLeft[i]))
+            {
+                //If no other keys have triggered the left event and a left key is pressed, trigger pressed event
+                if (LeftPresses == 0)
+                {
+                    EventManager.TriggerEvent("LeftKey", 1);
+                }
+                ++LeftPresses;
+            }
+
+            if (Input.GetKeyUp(MoveLeft[i]))
+            {
+                //If there aren't any other pressed left keys when we release a left key, trigger released event
+                if (LeftPresses == 1)
+                {
+                    EventManager.TriggerEvent("LeftKey", 0);
+                }
+                --LeftPresses;
+            }
+        }
+    }
+
+    private void RightKeys()
+    {
+        for (int i = 0; i < MoveRight.Length; ++i)
+        {
+            if (Input.GetKeyDown(MoveRight[i]))
+            {
+                //If no other keys have triggered the right event and a right key is pressed, trigger pressed event
+                if (RightPresses == 0)
+                {
+                    EventManager.TriggerEvent("RightKey", 1);
+                }
+                ++RightPresses;
+            }
+
+            if (Input.GetKeyUp(MoveRight[i]))
+            {
+                //If there aren't any other pressed right keys when we release a right key, trigger released event
+                if (RightPresses == 1)
+                {
+                    EventManager.TriggerEvent("RightKey", 0);
+                }
+                --RightPresses;
+            }
+        }
+    }
+
+    private void DownKeys()
+    {
+        for (int i = 0; i < MoveDown.Length; ++i)
+        {
+            if (Input.GetKeyDown(MoveDown[i]))
+            {
+                //If no other keys have triggered the down event and a down key is pressed, trigger pressed event
+                if (DownPresses == 0)
+                {
+                    EventManager.TriggerEvent("DownKey", 1);
+                }
+                ++DownPresses;
+            }
+
+            if (Input.GetKeyUp(MoveDown[i]))
+            {
+                //If there aren't any other pressed down keys when we release a down key, trigger released event
+                if (DownPresses == 1)
+                {
+                    EventManager.TriggerEvent("DownKey", 0);
+                }
+                --DownPresses;
+            }
+        }
+    }
+
+    private void InteractKeys()
+    {
+        for (int i = 0; i < Interact.Length; ++i)
+        {
+            if (Input.GetKeyDown(Interact[i]))
+            {
+                //If no other keys have triggered the interact event and an interact key is pressed, trigger pressed event
+                if (InteractPresses == 0)
+                {
+                    EventManager.TriggerEvent("InteractKey", 1);
+                }
+                ++InteractPresses;
+            }
+
+            if (Input.GetKeyUp(Interact[i]))
+            {
+                //If there aren't any other pressed interact keys when we release an interact key, trigger released event
+                if (InteractPresses == 1)
+                {
+                    EventManager.TriggerEvent("InteractKey", 0);
+                }
+                --InteractPresses;
+            }
+        }
+    }
+}
