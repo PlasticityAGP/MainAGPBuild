a19e61c813fea5d909a7ad9d8b2f43818b4d67c8
James Collins <jamescol@usc.edu>
2018-05-14 11:14:16 -0700

    Created and tested a scene loader that additively loads scenes

---
+++ Plasticity/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("A list of scenes we want to load in an additive and asynchronous manner to the current scene")]
+    private string[] LevelArray;
+
+
+	void Start () {
+        LoadScenes();
+	}
+	
+
+	void Update () {
+		
+	}
+
+
+    //Called in Start(), loads specified scenes.
+    private void LoadScenes()
+    {
+        //If no scenes are specified in the inspector, we don't do anything and return
+        if (LevelArray.Length == 0) return;
+
+        //Loop through LevelArray scenes specified in the inspector and load all scenes in an 
+        //additive and asynchronous manner to the current level 
+        for (int i = 0; i < LevelArray.Length; ++i)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(LevelArray[i], UnityEngine.SceneManagement.LoadSceneMode.Additive);
+        }
+        
+    }
+}
