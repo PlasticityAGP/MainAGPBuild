0e4092a62cd595d8859aa109c34dacd99dd40b2c
James Collins <jamescol@usc.edu>
2018-05-14 12:07:23 -0700

    started event manager

---
+++ Plasticity/Assets/EventManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class EventManager : MonoBehaviour {
+
+    private Dictionary<string, UnityEvent> EventDictionary;
+
+
+}
