895e04d20b252764b6692db4eb0f0db8f7de2f85
James Collins <jamescol@usc.edu>
2018-08-08 14:36:52 -0700

    Consolodated transform trigger and world trigger into one scripe/prefab. Added crate mesh to good box in puzzle

--- Plasticity/Assets/Scripts/SCR_LevelTrigger.cs
+++ Plasticity/Assets/Scripts/SCR_LevelTrigger.cs
@@ -34,16 +34,40 @@ public class SCR_LevelTrigger : MonoBehaviour {
     [SerializeField]
     [ShowIf("WriteToStateData")]
     private DataPair[] StatesToChange;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
+    [SerializeField]
+    [ShowIf("ThisTrigger", TypeOfTrigger.CharacterTransform)]
+    private bool TransformX;
+    [SerializeField]
+    [ShowIf("ThisTrigger", TypeOfTrigger.CharacterTransform)]
+    private bool TransformY;
+    [SerializeField]
+    [ShowIf("ThisTrigger", TypeOfTrigger.CharacterTransform)]
+    private bool TransformZ;
+    [SerializeField]
+    [ShowIf("TransformX")]
+    private float XPosition;
+    [SerializeField]
+    [ShowIf("TransformY")]
+    private float YPosition;
+    [SerializeField]
+    [ShowIf("TransformZ")]
+    private float ZPosition;
+    [SerializeField]
+    [ShowIf("TransformX")]
+    private float XLerpSpeed;
+    [SerializeField]
+    [ShowIf("TransformY")]
+    private float YLerpSpeed;
+    [SerializeField]
+    [ShowIf("TransformZ")]
+    private float ZLerpSpeed;
+    private bool LerpBool;
+    SCR_CharacterManager Manager;
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (LerpBool) DoLerp(Time.deltaTime);
+    }
 
     private bool IsIn(string Item, string[] Array)
     {
@@ -70,6 +94,14 @@ public class SCR_LevelTrigger : MonoBehaviour {
                 SCR_EventManager.TriggerEvent("SceneStateTrigger", PuzzleNumber);
             }
         }
+        else if(ThisTrigger == TypeOfTrigger.CharacterTransform)
+        {
+            if (other.tag == "Character")
+            {
+                Manager = other.GetComponentInChildren<SCR_CharacterManager>();
+                LerpBool = true;
+            }
+        }
 
         if (WriteToStateData)
         {
@@ -78,6 +110,56 @@ public class SCR_LevelTrigger : MonoBehaviour {
                 if(other.tag == StatesToChange[i].Tag)
                     LevelData.PuzzleStates[StatesToChange[i].IndexToWrite] = StatesToChange[i].Value;
             }
+        }
+    }
+
+    private void DoLerp(float DeltaTime)
+    {
+        float MagX = XPosition - Manager.gameObject.transform.position.x;
+        float MagY = YPosition - Manager.gameObject.transform.position.y;
+        float MagZ = ZPosition - Manager.gameObject.transform.position.z;
+        bool DoneX = false;
+        bool DoneY = false;
+        bool DoneZ = false;
+        if (Mathf.Abs(MagX) < 0.01f) DoneX = true;
+        else
+        {
+            if (TransformX)
+            {
+                float Dir = MagX / Mathf.Abs(MagX);
+                Vector3 NewPos = Manager.gameObject.transform.position;
+                NewPos.x += Dir * XLerpSpeed * DeltaTime;
+                Manager.gameObject.transform.position = NewPos;
+            }
+            else DoneX = true;
+        }
+        if (Mathf.Abs(MagY) < 0.01f) DoneY = true;
+        else
+        {
+            if (TransformY)
+            {
+                float Dir = MagY / Mathf.Abs(MagY);
+                Vector3 NewPos = Manager.gameObject.transform.position;
+                NewPos.y += Dir * YLerpSpeed * DeltaTime;
+                Manager.gameObject.transform.position = NewPos;
+            }
+            else DoneY = true;
+        }
+        if (Mathf.Abs(MagZ) < 0.01f) DoneZ = true;
+        else
+        {
+            if (TransformZ)
+            {
+                float Dir = MagZ / Mathf.Abs(MagZ);
+                Vector3 NewPos = Manager.gameObject.transform.position;
+                NewPos.z += Dir * ZLerpSpeed * DeltaTime;
+                Manager.gameObject.transform.position = NewPos;
+            }
+            else DoneZ = true;
+        }
+        if (DoneX && DoneY && DoneZ)
+        {
+            LerpBool = false;
         }
     }
 }
