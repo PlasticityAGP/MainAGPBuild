40db805c98503fb957abed6ac5fb6896fc1fe511
James Collins <jamescol@usc.edu>
2018-08-15 11:11:08 -0700

    Have basic AIController moving Guy via Timeline working. Moving on to triggering trapdoor from the AI

---
+++ Plasticity/Assets/Scripts/SCR_AIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+
+public class SCR_AIController : MonoBehaviour {
+
+    private UnityAction<int> InteractListener;
+    private bool Inside;
+    private PlayableDirector AIDirector;
+
+    private void Awake()
+    {
+        InteractListener = new UnityAction<int>(InteractPressed);
+    }
+
+    private void InteractPressed(int value)
+    {
+        if (value == 1 && Inside) DoTimeline();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            Inside = true;
+            SCR_EventManager.StartListening("InteractKey", InteractListener);
+        }    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            Inside = false;
+            SCR_EventManager.StopListening("InteractKey", InteractListener);
+        }
+    }
+
+    private void DoTimeline()
+    {
+        AIDirector.Play();
+    }
+
+    // Use this for initialization
+    void Start () {
+        AIDirector = gameObject.GetComponentInChildren<PlayableDirector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
